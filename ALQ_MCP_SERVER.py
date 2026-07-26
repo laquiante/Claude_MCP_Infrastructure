92c1506d8587aad2efade3a972df07ce628304e6
laquiante <33266194+laquiante@users.noreply.github.com>
2026-07-26 10:24:26 +0200

    Update ALQ_MCP_SERVER.py

--- ALQ_MCP_SERVER.py
+++ ALQ_MCP_SERVER.py
@@ -4,3 +4,10 @@ from fastmcp import FastMCP
 from dotenv import load_dotenv
 from scrapli import AsyncScrapli
 from pydantic import BaseModel, Field
+
+# C2950
+# C3620
+# C2611
+# 2511
+
+# minisforum workstation
